9d7227f1f7e97a9fef64e9e3db11edf82abf32ed
DieterDraelants <dieter.draelants@student.ucll.be>
2019-11-20 16:25:50 +0100

    Article class done

--- src/model/Article.java
+++ src/model/Article.java
@@ -1,11 +1,76 @@
 package model;
 
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
 public class Article {
     private int articleCode, quantity;
-    private String description, group;
+    private String articleName, group;
     private double price;
 
-    public Article() {
+    public Article(int articleCode, String name, String group, double price, int quantity) {
+        setArticleCode(articleCode);
+        setArticleName(name);
+        setGroup(group);
+        setPrice(price);
+        setQuantity(quantity);
+    }
 
+    public int getArticleCode() {
+        return articleCode;
+    }
+
+    public void setArticleCode(int articleCode) {
+        if (articleCode <= 0) {
+            throw new IllegalArgumentException("Article code cannot be zero or less");
+        }
+        this.articleCode = articleCode;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("You can't add 0 or less articles");
+        }
+        this.quantity = quantity;
+    }
+
+    public String getArticleName() {
+        return articleName;
+    }
+
+    public void setArticleName(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("You should give your product a name");
+        }
+        this.articleName = name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        String pattern = "^gr[1-9][0-9]*$";
+        if (group.trim().isEmpty()) {
+            throw new IllegalArgumentException("Your product should be in a group");
+        }
+        if (!group.matches(pattern)) {
+            throw new IllegalArgumentException("Your group notation doesn't match the required notation \n it should be like this: ^gr[1-9][0-9]*$");
+        }
+        this.group = group;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("Je prijs kan niet negatief zijn");
+        }
+        this.price = price;
     }
 }
