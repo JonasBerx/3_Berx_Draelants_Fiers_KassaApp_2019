3a3aefd27515206d6ba4b05c1f8ee983fef593ca
DieterDraelants <dieter.draelants@student.ucll.be>
2019-11-20 16:06:41 +0100

    reader

--- src/model/Article.java
+++ src/model/Article.java
@@ -1,4 +1,11 @@
 package model;
 
 public class Article {
+    private int articleCode, quantity;
+    private String description, group;
+    private double price;
+
+    public Article() {
+
+    }
 }
