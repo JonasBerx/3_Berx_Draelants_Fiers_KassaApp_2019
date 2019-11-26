5e072c65d167f13ee36df8776d939455a044933e
JonasBerx <jonas.berx1@student.ucll.be>
2019-11-26 19:26:10 +0100

    Finished load method in ExcelAdapter Added possibility for DBinMemory to add multiple products at once

--- src/model/ExcelAdapter.java
+++ src/model/ExcelAdapter.java
@@ -7,20 +7,21 @@ import jxl.read.biff.BiffException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 
 
 /**
 * @Author Jonas Berx
-* @Version 1.0
+* @Version 1.1
 * */
 public class ExcelAdapter implements LoadSaveStrategy {
-    ArticleDbInMemory dbInMemory;
-    ExcelPlugin plugin;
-    File articles;
+    private ArticleDbInMemory dbInMemory;
+    private ExcelPlugin plugin;
+    private File articles;
 
 
 
-    public ExcelAdapter() {
+    ExcelAdapter() {
         plugin = new ExcelPlugin();
         dbInMemory = new ArticleDbInMemory();
         articles = new File("src/bestanden/articles.xls");
@@ -31,9 +32,14 @@ public class ExcelAdapter implements LoadSaveStrategy {
     @Override
     public void load() {
         try {
-
             for (ArrayList<String> a : plugin.read(articles)) {
-                System.out.println(a);
+                //For testing purpose
+                System.out.println(a.get(0) + "," + a.get(1) + "," + a.get(2) + "," + a.get(3) + "," + a.get(4));
+
+                Article newArticle = new Article(Integer.parseInt(a.get(0)),
+                        a.get(1), a.get(2), Double.parseDouble(a.get(3)), Integer.parseInt(a.get(4)));
+                dbInMemory.addToMap(newArticle);
+
             }
         } catch (BiffException | IOException e) {
             e.printStackTrace();
