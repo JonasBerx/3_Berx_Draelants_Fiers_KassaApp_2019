8292501207ac3afb15c5622f056e4f4c806ba5a4
DieterDraelants <dieter.draelants@student.ucll.be>
2019-11-27 09:56:07 +0100

    Table is functional, just some minor tweaking

--- src/view/panels/CashierProductOverviewPane.java
+++ src/view/panels/CashierProductOverviewPane.java
@@ -1,41 +1,32 @@
 package view.panels;
 
-import database.ArticleDbInMemory;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
 import model.Article;
 import model.CsvLoadSave;
-import model.LoadSaveStrategy;
-import sun.util.locale.provider.JRELocaleProviderAdapter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 /**
  * @Author Dieter Draelants
  * Uitlezen van hashmap naar de artikelen pagina
- * - Change to
  */
 //TODO Change to TableVIEW!
 
 public class CashierProductOverviewPane extends GridPane {
-	//private TableView<Product> table;
-	CsvLoadSave strategy = new CsvLoadSave();
+	private CsvLoadSave strategy = new CsvLoadSave();
 	File file = new File("src/bestanden/articles.csv");
 
 
 
 	public CashierProductOverviewPane() throws FileNotFoundException {
-
-
+		//Load articles from chosen filetype
 		strategy.load();
 
 		this.setPadding(new Insets(5, 5, 5, 5));
@@ -43,29 +34,35 @@ public class CashierProductOverviewPane extends GridPane {
         this.setHgap(5);
         
 		this.add(new Label("Products:"), 0, 0, 1, 1);
-		this.add(new Label("Code"), 0, 1, 1, 1);
-		this.add(new Label("Name"), 1, 1, 1, 1);
-		this.add(new Label("Group"), 2, 1, 1, 1);
-		this.add(new Label("Price"), 3, 1, 1, 1);
-		this.add(new Label("Stock"), 4, 1, 1, 1);
 
 
+		//Making all columns
+		TableColumn<Article, Integer> codeCol = new TableColumn<>("Article Code");
+		TableColumn<Article, String> nameCol = new TableColumn<>("Article Name");
+		TableColumn<Article, String> groupCol = new TableColumn<>("Group");
+		TableColumn<Article, Double> priceCol = new TableColumn<>("Price");
+		TableColumn<Article, Integer> stockCol = new TableColumn<>("Stock");
 
+		//Setting the data value for the table
+		codeCol.setCellValueFactory(new PropertyValueFactory("articleCode"));
+		nameCol.setCellValueFactory(new PropertyValueFactory("articleName"));
+		groupCol.setCellValueFactory(new PropertyValueFactory("group"));
+		priceCol.setCellValueFactory(new PropertyValueFactory("price"));
+		stockCol.setCellValueFactory(new PropertyValueFactory("quantity"));
 
-		int i = 0;
-		for (Article a : strategy.getMemory().values() ) {
-			this.add(new Label(String.valueOf(a.getArticleCode())), 0, i + 2, 1, 1);
-			this.add(new Label(a.getArticleName()), 1, i + 2, 1, 1);
-			this.add(new Label(a.getGroup()), 2, i + 2, 1, 1);
-			this.add(new Label(String.valueOf(a.getPrice())), 3, i + 2, 1, 1);
-			this.add(new Label(String.valueOf(a.getQuantity())), 4, i + 2, 1, 1);
-
-			i++;
-		}
-
+		//adding Everything together
+		TableView<Article> table = new TableView<>();
+		table.setItems(getKkrList());
+		table.getColumns().addAll(codeCol, nameCol, groupCol, priceCol, stockCol);
+		this.add(table, 0, 0);
 
 	}
-	
-	
+
+	//Function that handles the data for the table
+	private ObservableList<Article> getKkrList() {
+		ObservableList<Article> articles = FXCollections.observableArrayList();
+		articles.addAll(strategy.getMemory().values());
+		return articles;
+	}
 
 }
